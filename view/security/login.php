5c95d75368625c29c1a4b7e1856a94154ee4d418
Forum_framework <florian.leininger01@gmail.com>
2023-03-14 13:56:25 +0100

    + creation page connexion et inscription

---
+++ view/security/login.php
@@ -0,0 +1,21 @@
+<h1>Connexion</h1>
+
+<form>
+    <p>
+        <label>Identifiant :<br>
+            <input type="email" name="email" required/>
+        </label>
+    </p>
+
+    <p>
+        <label>Mot de passe :<br>
+            <input type="text" name="mdp" required/>
+        </label>
+    </p>
+
+    <p>
+        <label>
+            <input type="submit" name="submit" value="Valider"/>
+        </label>
+    </p>
+</form>
