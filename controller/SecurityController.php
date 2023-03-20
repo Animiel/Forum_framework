b4f9c93fcf2d0f7342c405f95519a9cb211839fa
Forum_framework <florian.leininger01@gmail.com>
2023-03-20 14:34:30 +0100

    + ajout register

--- controller/SecurityController.php
+++ controller/SecurityController.php
@@ -5,25 +5,44 @@
     use App\Session;
     use App\AbstractController;
     use App\ControllerInterface;
+    use Model\Managers\UserManager;
 
     class SecurityController extends AbstractController implements ControllerInterface {
         
         public function index() {}
 
         public function inscription() {
+            $message = "";
+            $userManager = new UserManager();
             if (isset($_POST['submit'])) {
                 $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
                 $mdp = filter_input(INPUT_POST, "mdp", FILTER_SANITIZE_SPECIAL_CHARS);
                 $mdpConf = filter_input(INPUT_POST, "mdp_confirm", FILTER_SANITIZE_SPECIAL_CHARS);
                 $pseudo = filter_input(INPUT_POST, "pseudo", FILTER_SANITIZE_SPECIAL_CHARS);
 
-                if ($email && ($mdp && strlen($mdp) >= 8 && $mdp === $mdpConf) && $pseudo) {
-                    
-                    ["mot_de_passe" => password_hash($mdp, PASSWORD_DEFAULT),
-                    "pseudo" => $pseudo,
-                    "email" => $email];
-
+                if ($email && $userManager->emailCheck($email) == null) {
+                    if ($pseudo && $userManager->pseudoCheck($pseudo) == null) {
+                        if ($mdp && $mdp === $mdpConf && strlen($mdp) >= 8) {
+                            $data = ["mot_de_passe" => password_hash($mdp, PASSWORD_DEFAULT),
+                                "pseudo" => $pseudo,
+                                "email_membre" => $email
+                            ];
+                            $userManager->add($data);
+                            $message = "Utilisateur ajouté.";
+                        }
+                        else {
+                            $message = "Mot de passe invalide.";
+                        }
+                    }
+                    else {
+                        $message = "Pseudo déjà utilisé.";
+                    }
+                }
+                else {
+                    $message = "Email déjà utilisé.";
                 }
             }
+            echo $message;
+            $this->redirectTo("florian_LEININGER", "Forum_framework", "index");
         }
     }
--- model/managers/UserManager.php
+++ model/managers/UserManager.php
@@ -15,5 +15,25 @@
             parent::connect();
         }
 
+        public function emailCheck($email) {
+            $sql = "SELECT *
+                    FROM ".$this->tableName." u
+                    WHERE u.email_membre = :email";
+
+            return $this->getOneOrNullResult(
+                DAO::select($sql, [':email' => $email]),
+                $this->className);
+        }
+
+        public function pseudoCheck($pseudo) {
+            $sql = "SELECT *
+                    FROM ".$this->tableName." u
+                    WHERE u.pseudo = :pseudo";
+
+            return $this->getOneOrNullResult(
+                DAO::select($sql, [':pseudo' => $pseudo]),
+                $this->className);
+        }
+
 
     }
